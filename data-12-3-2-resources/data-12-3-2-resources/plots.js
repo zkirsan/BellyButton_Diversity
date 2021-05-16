673e13eef42ab6baf35cc95eca5c889944a1f126
unknown <zeynelkirsan@gmail.com>
2021-05-16 00:03:20 -0400

    Added third resource file

---
+++ data-12-3-2-resources/data-12-3-2-resources/plots.js
@@ -0,0 +1,26 @@
+d3.json("samples.json").then(function(data) {
+    console.log(data);
+  });
+ 
+  
+// // to extract only the wfreq
+// d3.json("samples.json").then(function(data){
+//     wfreq = data.metadata.map(person => person.wfreq);
+//     console.log(wfreq);
+// });
+
+// // to sort the wfreq array in descending order
+// d3.json("samples.json").then(function(data){
+//     wfreq = data.metadata.map(person =>
+// person.wfreq).sort((a,b) => b - a);
+//     console.log(wfreq);
+// });
+
+// // to delete null values from the sorted wfreq array
+// d3.json("samples.json").then(function(data){
+//     wfreq = data.metadata.map(person =>
+// person.wfreq).sort((a,b) => b - a);
+//     filteredWfreq = wfreq.filter(element => element !=
+// null);
+//     console.log(filteredWfreq);
+// });
