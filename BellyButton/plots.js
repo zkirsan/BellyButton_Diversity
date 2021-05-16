3f70aa23e02212eb6e9aad9e7c1ba07f430278a3
unknown <zeynelkirsan@gmail.com>
2021-05-16 01:18:35 -0400

    Added BellyButton/ file

---
+++ BellyButton/plots.js
@@ -0,0 +1,69 @@
+// d3.json("samples.json").then(function(data) {
+//     console.log(data);
+//   });
+ 
+  
+// // to extract only the wfreq
+// d3.json("samples.json").then(function(data){
+//     wfreq = data.metadata.map(person => person.wfreq);
+//     console.log(wfreq);
+// });
+
+// // to sort the wfreq array in descending order
+// d3.json("samples.json").then(function(data){
+//     wfreq = data.metadata.map(person =>
+// person.wfreq).sort((a,b) => b - a);
+//     console.log(wfreq);
+// });
+
+// // to delete null values from the sorted wfreq array
+// d3.json("samples.json").then(function(data){
+//     wfreq = data.metadata.map(person =>
+// person.wfreq).sort((a,b) => b - a);
+//     filteredWfreq = wfreq.filter(element => element !=
+// null);
+//     console.log(filteredWfreq);
+// });
+
+// // display the metadata of any individual from the dataset
+// d3.json("samples.json").then(function(data){
+//   firstPerson = data.metadata[0];
+//   Object.entries(firstPerson).forEach(([key, value]) =>
+//     {console.log(key + ': ' + value);});
+// });
+
+function init() {
+  var selector = d3.select("#selDataset");
+
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+})}
+
+init();
+
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
+
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    })
+    
+  });
+}
